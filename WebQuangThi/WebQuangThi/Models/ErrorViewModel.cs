cb0d00d371b1b505fccc2a2ceb72de135419cd35
quang709 <69231857+quang709@users.noreply.github.com>
2020-11-10 08:42:45 +0700

    Thi Web

---
+++ WebQuangThi/WebQuangThi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebQuangThi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
